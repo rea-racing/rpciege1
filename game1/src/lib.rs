a5d20946a019fdef5351b083796100d299d6f959
rea-racing <realracing34game@gmail.com>
2023-06-17 14:02:00 +0000

    create & modif

--- game1/src/lib.rs
+++ game1/src/lib.rs
@@ -1,3 +1,29 @@
+#![no_std]
+use crate::interface::RPCiege5SwapCallbackTrait;
+use soroban_sdk::{contractimpl, contracttype, Address, Env};
+
+pub struct RPCiege5SwapCallback;
+
+#[contracttype]
+pub enum DataKey {
+    LiquidityPool,
+}
+
+#[allow(unused_variables)]
+#[contractimpl]
+impl RPCiege5SwapCallbackTrait for RPCiege5SwapCallback {
+    fn swap_callback(
+        env:Env,
+        liqpool:Address,
+        token_id:Address,
+        amount:i128,
+        initiator:Option<Address>,
+    ) {
+        // no
+    }
+    
+}
+
 // #![no_std]
 
 // use soroban_sdk::{contractimpl, Address, Env ,String};
@@ -14,47 +40,47 @@
 // }
 
 // ------------------------------------------------------------
-#![no_std]
-use soroban_sdk::{
-    contracterror, contractimpl, panic_with_error, xdr::ToXdr, Address, Env, Symbol,
-};
-pub struct Contract;
-#[contracterror]
-#[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
-#[repr(u32)]
-pub enum Error {
-    MissingPew = 1,
-    UsedPew = 2,
-}
-#[contractimpl]
-impl Contract {
-    pub fn game_3(env: Env,  _nft_dest: Address) -> Result<(), Error> {
-        let symbol = Symbol::new(&env, "1702871");
+// #![no_std]
+// use soroban_sdk::{
+//     contracterror, contractimpl, panic_with_error, xdr::ToXdr, Address, Env, Symbol,
+// };
+// pub struct Contract;
+// #[contracterror]
+// #[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
+// #[repr(u32)]
+// pub enum Error {
+//     MissingPew = 1,
+//     UsedPew = 2,
+// }
+// #[contractimpl]
+// impl Contract {
+//     pub fn game_3(env: Env,  _nft_dest: Address) -> Result<(), Error> {
+//         let symbol = Symbol::new(&env, "1702871");
 
-        if env.storage().has(&symbol) {
-            panic_with_error!(env, Error::UsedPew);
-        }
-        let bytes = symbol.clone().to_xdr(&env);
-        let hash = env.crypto().sha256(&bytes);
-        let mut i = 0;
-        let mut has_pew = false;
-        for v in hash.clone().into_iter() {
-            if v == 112
-                && hash.get(i + 1).unwrap_or(0) == 101
-                && hash.get(i + 2).unwrap_or(0) == 119
-            {
-                has_pew = true;
-            }
-            i += 1;
-        }
-        if !has_pew {
-            panic_with_error!(env, Error::MissingPew);
-        } else {
-            env.storage().set(&symbol, &true);
-        }
-        Ok(())
-    }
-}
+//         if env.storage().has(&symbol) {
+//             panic_with_error!(env, Error::UsedPew);
+//         }
+//         let bytes = symbol.clone().to_xdr(&env);
+//         let hash = env.crypto().sha256(&bytes);
+//         let mut i = 0;
+//         let mut has_pew = false;
+//         for v in hash.clone().into_iter() {
+//             if v == 112
+//                 && hash.get(i + 1).unwrap_or(0) == 101
+//                 && hash.get(i + 2).unwrap_or(0) == 119
+//             {
+//                 has_pew = true;
+//             }
+//             i += 1;
+//         }
+//         if !has_pew {
+//             panic_with_error!(env, Error::MissingPew);
+//         } else {
+//             env.storage().set(&symbol, &true);
+//         }
+//         Ok(())
+//     }
+// }
 
-#[cfg(test)]
-mod test;
+// #[cfg(test)]
+// mod test;
--- game1/src/test.rs
+++ game1/src/test.rs
@@ -1,96 +1,96 @@
-#![cfg(test)]
+// #![cfg(test)]
 
-use std::println;
+// use std::println;
 
-// use super::{Contract, ContractClient, Address};
-use alloc::string::ToString;
-use soroban_sdk::{Env, Symbol, xdr::ToXdr};
+// // use super::{Contract, ContractClient, Address};
+// use alloc::string::ToString;
+// use soroban_sdk::{Env, Symbol, xdr::ToXdr};
 
-extern crate alloc;
-extern crate std;
+// extern crate alloc;
+// extern crate std;
+
+// // #[test]
+// // fn test() {
+// //     let env = Env::default();
+// //     let contract_id = env.register_contract(None, Contract);
+// //     let client = ContractClient::new(&env, &contract_id);
+
+// //     let nft_dest = Address::random(&env);
+
+// // }
 
 // #[test]
-// fn test() {
+// #[ignore]
+// fn generate_pew_symbol() {
 //     let env = Env::default();
-//     let contract_id = env.register_contract(None, Contract);
-//     let client = ContractClient::new(&env, &contract_id);
+//         env.budget().reset_unlimited();
 
-//     let nft_dest = Address::random(&env);
+//     let mut iters: i32 = 0;
+//     let mut has_pew = false;
+//     let mut symbol = Symbol::new(&env,"");
 
+//     while !has_pew {
+//         symbol = Symbol::new(&env, iters.clone().to_string().as_str());
+
+//         let bytes = symbol.clone().to_xdr(&env);
+//         let hash = env.crypto().sha256(&bytes);
+//         let mut i = 0;
+
+//         for v in hash.clone().into_iter() {
+//             if v == 112
+//                 && hash.get(i + 1).unwrap_or(0) == 101
+//                 && hash.get(i + 2).unwrap_or(0) == 119
+//             {
+//                 has_pew = true;
+
+//                 if has_pew {
+//                     println!("has_pew: {:?}", has_pew);
+//                     println!("{:?}", bytes);
+//                     println!("{:?}", hash);
+//                 }
+//             }
+//             i += 1;
+//         }
+
+//         // if iters % 10000 == 0 {
+//         //     // println!("{:?}", &hash);
+//         //     println!("{:?}", &iters);
+//         // }
+
+//         // if iters >= 100000 {
+//         //     break;
+//         // }
+
+//         iters += 1;
+//     }
+
+//     println!("has_pew: {:?}", has_pew);
+//     println!("{:?}", symbol);
+    
 // }
 
-#[test]
-#[ignore]
-fn generate_pew_symbol() {
-    let env = Env::default();
-        env.budget().reset_unlimited();
+// #[test]
+// #[ignore]
+// fn generate_pew_symbol_2() {
+//     let env = Env::default();
+//         env.budget().reset_unlimited();
 
-    let mut iters: i32 = 0;
-    let mut has_pew = false;
-    let mut symbol = Symbol::new(&env,"");
+//     let symbol = Symbol::new(&env,"1702871");
 
-    while !has_pew {
-        symbol = Symbol::new(&env, iters.clone().to_string().as_str());
-
-        let bytes = symbol.clone().to_xdr(&env);
-        let hash = env.crypto().sha256(&bytes);
-        let mut i = 0;
-
-        for v in hash.clone().into_iter() {
-            if v == 112
-                && hash.get(i + 1).unwrap_or(0) == 101
-                && hash.get(i + 2).unwrap_or(0) == 119
-            {
-                has_pew = true;
-
-                if has_pew {
-                    println!("has_pew: {:?}", has_pew);
-                    println!("{:?}", bytes);
-                    println!("{:?}", hash);
-                }
-            }
-            i += 1;
-        }
-
-        // if iters % 10000 == 0 {
-        //     // println!("{:?}", &hash);
-        //     println!("{:?}", &iters);
-        // }
-
-        // if iters >= 100000 {
-        //     break;
-        // }
-
-        iters += 1;
-    }
-
-    println!("has_pew: {:?}", has_pew);
-    println!("{:?}", symbol);
-    
-}
-
-#[test]
-#[ignore]
-fn generate_pew_symbol_2() {
-    let env = Env::default();
-        env.budget().reset_unlimited();
-
-    let symbol = Symbol::new(&env,"1702871");
-
-    let bytes = symbol.clone().to_xdr(&env);
-    let hash = env.crypto().sha256(&bytes);
-    let mut i = 0;
-    let mut has_pew = false;
-    for v in hash.clone().into_iter() {
-        if v == 112
-            && hash.get(i + 1).unwrap_or(0) == 101
-            && hash.get(i + 2).unwrap_or(0) == 119
-        {
-            has_pew = true;
-            println!("{:?}", bytes);
-            println!("{:?}", hash);
-        }
-        i += 1;
-    }
-    println!("has_pew: {:?}", has_pew);
-}
+//     let bytes = symbol.clone().to_xdr(&env);
+//     let hash = env.crypto().sha256(&bytes);
+//     let mut i = 0;
+//     let mut has_pew = false;
+//     for v in hash.clone().into_iter() {
+//         if v == 112
+//             && hash.get(i + 1).unwrap_or(0) == 101
+//             && hash.get(i + 2).unwrap_or(0) == 119
+//         {
+//             has_pew = true;
+//             println!("{:?}", bytes);
+//             println!("{:?}", hash);
+//         }
+//         i += 1;
+//     }
+//     println!("has_pew: {:?}", has_pew);
+// }
